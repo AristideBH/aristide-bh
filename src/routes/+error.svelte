28cb383444b75c3c7399deb70be519dd51bab640
AristideBH <aristide.bruneau@gmail.com>
2026-04-28 15:47:42 +0200

    fix: refactor error page layout and remove unused components for improved clarity

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
 	import { page } from '$app/state';
-	import * as Card from '$lib/components/ui/card';
-	import { Button } from '$lib/components/ui/button';
-	import { ArrowLeftFromLine } from 'lucide-svelte';
 	import Marqueeck from '@arisbh/marqueeck';
 	import Section from '$lib/components/layout/Section.svelte';
+	import { dirhover } from '@arisbh/dirhover-svelte';
 
 	const status = structuredClone(page.status);
 	const error = structuredClone(page.error);
@@ -15,7 +13,34 @@
 	];
 </script>
 
-<Section content={{ width: 'full-width' }} class="full-width flex flex-col">
+<section class="flex flex-row">
+	<span class="~size-12/20" 
+	{@attach dirhover({
+				childClass:
+					'!~size-12/20 grid place-items-center group-hover:fill-white',
+				curtainClass: '!bg-primary'
+			})}>
+		4
+	</span>
+	<span class="~size-12/20" 
+	{@attach dirhover({
+				childClass:
+					'!~size-12/20 grid place-items-center group-hover:fill-white',
+				curtainClass: '!bg-primary'
+			})}>
+		0
+	</span>
+	<span class="~size-12/20" 
+	{@attach dirhover({
+				childClass:
+					'!~size-12/20 grid place-items-center group-hover:fill-white',
+				curtainClass: '!bg-primary'
+			})}>
+		4
+	</span>
+</section>
+
+<!-- <Section content={{ width: 'full-width' }} class="full-width flex flex-col">
 	{#each message as _, i}
 		<Marqueeck
 			class="{_.class} layout-full -rotate-3 bg-muted font-heading text-foreground"
@@ -24,37 +49,14 @@
 			{_.text}
 		</Marqueeck>
 	{/each}
-</Section>
+</Section> -->
 
 <section class="flex flex-col">
 	<!-- <pre>{JSON.stringify(error, null, 2)}</pre> -->
 	<p class="lead text-balance">
 		Cette page semble ne pas exister !<br /> Reprenez depuis la <a href="/"> page d'accueil</a>.
 	</p>
+
+	
 </section>
 
-<!-- <section>
-	<Card.Root class="bg-muted">
-		<Card.Header>
-			<h1>Oops !</h1>
-			<Card.Description>Erreur {status} : {error?.message}</Card.Description>
-		</Card.Header>
-		<Card.Content>
-			<p>Seems like the page you are looking doesn't exist !</p>
-		</Card.Content>
-		<Card.Footer class="flex gap-2">
-			<Button
-				variant="border"
-				size="icon"
-				title="Page précédente"
-				onclick={() => {
-					window.history.back();
-					return false;
-				}}
-			>
-				<ArrowLeftFromLine class="size-4" />
-			</Button>
-			<Button variant="default" href="/">Go back home</Button>
-		</Card.Footer>
-	</Card.Root>
-</section> -->
